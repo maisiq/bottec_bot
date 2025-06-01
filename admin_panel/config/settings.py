b88573a7bfa9ebb1a393f5a86a878e9c508e96ec
Vladislav Vasilev <vyruscd@gmail.com>
2025-06-01 23:42:51 +0400

    chore(admin): Add allowed hosts

--- admin_panel/config/settings.py
+++ admin_panel/config/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nginx', 'localhost']
 
 
 # Application definition
